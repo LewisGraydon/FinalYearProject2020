c0451a48fa26503895204e92cdacb14598d2e89e
Lewis Graydon <lewis.graydon@yahoo.com>
2020-04-07 17:45:49 +0100

    Finished collecting the second data set, also made a format which was operational within MatLab.

--- GPP.LG_SnakeArtefact/GPP.LG_SnakeTrainer/main.cpp
+++ GPP.LG_SnakeArtefact/GPP.LG_SnakeTrainer/main.cpp
@@ -4,13 +4,50 @@
 #include "ExternalSources/ALGLib-3.16.0/dataanalysis.h"
 #include "ExternalSources/ALGLib-3.16.0/stdafx.h"
 
+#include <fstream>
+#include <sstream>
+
 int main()
 {
 	alglib::mlptrainer trn;
 	alglib::mlpcreatetrainer(400, 4, trn);
 
-	alglib::real_2d_array xy = "[[0,0,0], [0,0,0], [0,0,0], [0,0,0]]";	// Okay, this should be the inputs from the data set. I.e. the first array will be the first data set, ending with a number
+	alglib::real_2d_array xy;	// Okay, this should be the inputs from the data set. I.e. the first array will be the first data set, ending with a number
 																		// between 1 and 3 as well as a ; on the end which will be used to tell the program to use next line.
+	xy.setlength(10828, 1601);
+
+	std::ifstream inFile("outputDataVersion2.txt");
+	
+	int count = 0;
+
+	std::string line;
+	while (std::getline(inFile, line,','))
+	{
+		std::cout << "The value is" << std::stof(line) << std::endl;
+		/*std::istringstream iss (line);
+		std::vector<double> vect;
+
+		for (int i; iss >> i;)
+		{
+			vect.push_back(i);
+			if (iss.peek() == ',')
+			{
+				iss.ignore();
+			}
+			if (iss.peek() == ';')
+			{
+				iss.ignore();
+			}
+
+			for (int j = 0; j < vect.size(); j++)
+			{
+				xy(count, j) = vect.at(j);
+			}
+
+			count++;
+		}		*/
+	}
+
 	alglib::mlpsetdataset(trn, xy, 4);
 
 	double stoppingCriteria = 0.000;
